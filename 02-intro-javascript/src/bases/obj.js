153520f1da634bdec91b12c810741986c9f2dbe2
estiven2011 <juan_posada23221@elpoli.edu.co>
2025-03-12 16:32:19 -0500

    ejercicio ojetos

--- 02-intro-javascript/src/bases/obj.js
+++ 02-intro-javascript/src/bases/obj.js
@@ -2,5 +2,13 @@ const biblioteca = {
     titulo: "Piense y hagase rico",
     autor: "Gabriel Garcia",
     anioPublicacion: 2010,
-    disponible: true
+    disponible: true,
+
+    isDisponible: function (obj){
+        obj.filter((ele) => {
+            return ele.disponible = true
+        })
+    }
 }
+
+console.log(biblioteca.isDisponible(biblioteca));
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,6 +1,15 @@
-const biblioteca = {
-    titulo: "Piense y hagase rico",
-    autor: "Gabriel Garcia",
-    anioPublicacion: 2010,
-    disponible: true
+const producto = {
+    nombre: 'Laptop',
+    precio: 2000,
+    disponible: true,
+    detalles: {
+        marca: 'mac',
+        peso: 1.0
+    }
 }
+
+function mostrarProductos (obj) {
+    return `Tu producto es ${obj.nombre}, Precio:${obj.precio}, Disponile:${obj.disponible}`
+}
+
+console.log(mostrarProductos(producto));
